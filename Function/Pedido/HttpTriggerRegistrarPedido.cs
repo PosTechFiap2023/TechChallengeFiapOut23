8389b84cb16b5edfeab72ca0cd5defb523d295b2
Demetrios <Demetrios@DESKTOP-2S6BMVF>
2024-01-08 20:37:05 -0300

    Commit Inicial

---
+++ Function/Pedido/HttpTriggerRegistrarPedido.cs
@@ -0,0 +1,79 @@
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+
+namespace Company.Function
+{
+    public class Pedidos : TableEntity{
+        // public string PertitionKey { get; set; }
+        // public string RowKey { get; set; }
+
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string UserEmail { get; set; }
+        public string Price { get; set; }
+
+        public bool IsAproved { get; set; }
+    }
+    public static class HttpTriggerRegistrarPedido
+    {
+        [FunctionName("HttpTriggerRegistrarPedido")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "novopedido")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject<Pedidos>(requestBody);
+            name = name ?? data?.name;
+
+            // Get Storage Information
+            var accountName = "rgfunctions9fe2";
+            var accountKey = Environment.GetEnvironmentVariable("AzureStoregeKey");
+
+            // Set Auth
+            var creds = new StorageCredentials(accountName, accountKey);
+            var account = new CloudStorageAccount(creds, useHttps: true);
+
+            // Connect to Storage
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("pedidos");
+
+            Pedidos obj = new Pedidos()
+            {
+                PartitionKey = Guid.NewGuid().ToString(), // Must be unique
+                RowKey = Guid.NewGuid().ToString(), // Must be unique
+                OrderId = data?.OrderId,
+                ProductId = data?.ProductId,
+                UserEmail = data?.UserEmail,
+                Price = data?.Price,
+                IsAproved = data?.IsAproved,
+            };
+
+            var insertOperation = TableOperation.Insert(obj);
+            await table.ExecuteAsync(insertOperation);
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response." + data
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+
+        }
+    }
+}
