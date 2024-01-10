14ff6c3afbfdd74984ef82e266a91abb5972728a
Demetrios <Demetrios@DESKTOP-2S6BMVF>
2024-01-09 22:00:57 -0300

    VersaoFinalAconfirmar

--- Function/Pedido/HttpTriggerRegistrarPedido.cs
+++ Function/Pedido/HttpTriggerRegistrarPedido.cs
@@ -1,5 +1,6 @@
 
-using System.Net.Http;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,12 +8,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.IO;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
-using System;
-using System.Collections.Generic;
 
 namespace Company.Function
 {
@@ -44,7 +42,7 @@ namespace Company.Function
 
             // Get Storage Information
             var accountName = "rgfunctions9fe2";
-            var accountKey = Environment.GetEnvironmentVariable("AzureStoregeKey");
+            var accountKey = Environment.GetEnvironmentVariable("AzureStorageKey");
 
             // Set Auth
             var creds = new StorageCredentials(accountName, accountKey);
